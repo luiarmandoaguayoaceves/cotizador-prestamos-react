28a403f2390a7125a0774bf353b62ff88d84bfa1
Luis Armando Aguayo A <luisarmandoaguayoaceves@gmail.com>
2024-01-23 10:46:16 -0600

    botones

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import {useState} from "react";
-import Header from "./components/Header"
+import { useState } from "react";
+import Header from "./components/Header";
 
 function App() {
   const [cantidad, setCantidad] = useState(10000);
@@ -7,25 +7,39 @@ function App() {
   const MAX = 20000;
   const STEP = 100;
 
-  function handleChange(e){
+  function handleChange(e) {
     setCantidad(+e.target.value);
   }
 
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
-      <Header/>
+      <Header />
 
-      <input 
-        type="range" 
+      <div className="flex justify-between my-6">
+        <button
+          type="button"
+          className="h-10 w-10 flex items-center justify-center font-bold text-white text-2xl bg-lime-500 rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-lime-500"
+        >-</button>
+        <button
+          type="button"
+          className="h-10 w-10 flex items-center justify-center font-bold text-white text-2xl bg-lime-500 rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-lime-500"
+        >+</button>
+      </div>
+
+      <input
+        type="range"
         className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
         onChange={handleChange}
         min={MIN}
         max={MAX}
         step={STEP}
+        value={cantidad}
       />
-      <p className="text-center my-10 text-5xl font-extrabold text-indigo-600">{cantidad}</p>
+      <p className="text-center my-10 text-5xl font-extrabold text-indigo-600">
+        {cantidad}
+      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
