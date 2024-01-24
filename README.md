9aa269aeb7b7cec9c9ca077a65c274d2f18467d1
Luis Armando Aguayo Aceves <luisarmandoaguayoaceves@gmail.com>
2024-01-23 19:46:29 -0600

    deploid de proyecto terminado con react

--- README.md
+++ README.md
@@ -21,5 +21,7 @@
 5. los  props funcionan para pasarle como parametros llamados props a las etiquetas o componentes creados por separado
    los state's son los eventos que se agregan como funciones como handlers donde se usan como onClick, onChange, etc... para los eventos.
 
+6. UseEffect funcionan escuchando todo momento cambios en los valores se refomienda usar useEffect por separado apra cada actualizacion que necesites de esta manera se separan las los arreglos de dependencias para que sea mas comodo y no genera problemas haciendo el codigo mas limpio
 
-    
+
+7. para hacer el deployd se ejecuta el comando `npm run deploid` y genera una carpeta llamada dist que sera el proyecto minificado y listo para subir
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect} from "react";
 import Header from "./components/Header";
 import Button from "./components/Button";
+import { formatearDinero, calcularTotalPagar } from "./helpers";
 
 function App() {
   const [cantidad, setCantidad] = useState(10000);
+  const [meses, setMeses] = useState(6);
+  const [total, setTotal] = useState(0);
+  const [pago, setPago] = useState(0);
+
+  useEffect( () => {
+    const resultadoTotalPagar = calcularTotalPagar(cantidad, meses);
+    setTotal(resultadoTotalPagar)
+
+    
+  }, [cantidad, meses])
+
+  useEffect ( () => {
+    //Calcular el pago mensual
+    setPago(total/meses)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[total])
+
   const MIN = 0;
   const MAX = 20000;
   const STEP = 100;
@@ -59,8 +77,34 @@ function App() {
         value={cantidad}
       />
       <p className="text-center my-10 text-5xl font-extrabold text-indigo-600">
-        {cantidad}
+        {formatearDinero(cantidad)}
       </p>
+
+      <h2 className="text-2xl font-extrabold text-gray-500 text-center">
+        Elige un <span className="text-indigo-600">Plazo</span> a pagar
+      </h2>
+
+      <select
+        className="mt-5 w-full p-2 bg-white border border-gray-300 rounded-lg text-center text-xl font-bold text-gray-500"
+        value={meses}
+        onChange={ e => setMeses(+e.target.value)}
+      >
+        <option value="6">6 Meses</option>
+        <option value="12">12 Meses</option>
+        <option value="24">24 Meses</option> 
+      </select>
+
+      <div className="my-5 space-y-3 bg-gray-50 p-5">
+        <h2 className="text-2xl font-extrabold text-gray-500 text-center">
+          Resumen <span className="text-indigo-600">de pagos</span>
+        </h2>
+
+        <p className="text-xl text-gray-500 text-center font-bold"> {meses} Meses</p>
+        <p className="text-xl text-gray-500 text-center font-bold"> {formatearDinero(total)}Total a pagar</p>
+        <p className="text-xl text-gray-500 text-center font-bold"> {formatearDinero(pago)}Mensualidades</p>
+      </div>
+
+
     </div>
   );
 }
